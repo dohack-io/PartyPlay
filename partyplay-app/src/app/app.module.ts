1b0f638c6ec962c00ebea59a00a6fa365215b3be
TheJLifeX <44620382+TheJLifeX@users.noreply.github.com>
2019-03-23 15:40:02 +0100

    Add angular material.

--- partyplay-app/src/app/app.module.ts
+++ partyplay-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
